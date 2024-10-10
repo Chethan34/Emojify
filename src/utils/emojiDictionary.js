c6d132c1eaeab0902f116821372193126c815efb
Chethan <Chethanprakash720@gmail.com>
2024-10-10 11:55:40 +0530

    Emoji Dictionaries Added

---
+++ src/utils/emojiDictionary.js
@@ -0,0 +1,24 @@
+export default function emojiDictionary() {
+    return {
+      'love': '❤️',
+      'happy': '😊',
+      'sad': '😢',
+      'dog': '🐶',
+      'cat': '🐱',
+      'pizza': '🍕',
+      'good': '👍',
+      'bad': '👎',
+      'yes': '✅',
+      'no': '❌',
+      'hello': '👋',
+      'bye': '👋',
+      'laugh': '😂',
+      'cool': '😎',
+      'wow': '😮',
+      'angry': '😠',
+      'heart': '❤️',
+      'sleep': '😴',
+      'cry': '😢',
+      'think': '🤔'
+    };
+  }
